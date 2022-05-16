bdfe789f7335ec73b934bac9afd4523236572167
girnal82 <grinal82@gmail.com>
2022-05-17 02:31:30 +0300

    fixed bugs, added doc strings

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ NUMBER_OF_PHOTOS = 5
 
 
 class VkDownloader:
+    """
+    Class to operate with VK API
+    """
+
     host = r'https://api.vk.com'
 
     def __init__(self, token: str):
@@ -19,6 +23,9 @@ class VkDownloader:
         self.token = token
 
     def create_folder(self):
+        """
+        Method to create local folder to save downloaded files
+        """
         path = os.getcwd()
         new_folder = path + '/images'
         try:
@@ -29,6 +36,10 @@ class VkDownloader:
             print('folder successfully created')
 
     def get_vk_photo(self):
+        '''
+        Method to download profile photos from VK using VK API and to sort them 
+        in json file depending on their size
+        '''
         params = {
             'owner_id': "552934290",
             'access_token': self.token,
@@ -39,39 +50,59 @@ class VkDownloader:
         }
         url = f'{self.host}/method/photos.get'
         response = requests.get(
-            url, params)
-        pprint(response.json())
-        return response.json()
-
-    def sort_photos(self):
-        new_folder = self.create_folder()
-        data = self.get_vk_photo()
+            url, params).json()
+        # pprint(response)
         self.json_to_save = []
-        for files in data['response']['items']:
-            data_on_file = {}
-            data_on_file['file_name'] = files['likes']['count']
-            data_on_file['size'] = files['sizes'][-1]['type']
-            data_on_file['url'] = files['sizes'][-1]['url']
-            self.json_to_save.append(data_on_file)
-            files_url = files['sizes'][-1]['url']
-            file_name = files['likes']['count']
+        # Creating dict on files with identical number of likes
+        data_on_files = {}
+        for files in response['response']['items']:
+            if files['likes']['count'] in data_on_files:
+                data_on_files[files['likes']['count']] += 1
+            else:
+                data_on_files[files['likes']['count']] = 1
+
+        # Defining the largest photos
+        for files in response['response']['items']:
+            file_params = sorted(
+                files['sizes'], key=lambda x: x['height'] + x['width'], reverse=True)[0]
+
+            # Creating the name for the photo
+            if data_on_files.get(files['likes']['count'], 0) > 1:
+                file_params['file_name'] = str(
+                    files['likes']['count']) + '_' + str(files['date']) + '.jpg'
+            else:
+                file_params['file_name'] = str(
+                    files['likes']['count']) + '.jpg'
+
+            # Appending to resulting list (json_to_save)
+            self.json_to_save.append(file_params)
+        pprint(self.json_to_save)
+
+        self.create_folder()
+        """Downloading files into pre-created local folder"""
+        for file in self.json_to_save:
+            file_name = file.get('file_name')
+            url = file.get('url')
+            # Initializing progress bar
             bar = PixelBar('Processing', max=100)
             for i in range(100):
                 time.sleep(0.001)
                 bar.next()
-            bar.finish()
-            self.images = requests.get(files_url)
-
-            with open(f'images/{file_name}.jpeg', 'wb') as file:
-                file.write(self.images.content)
-
-            with open('data.json', 'w') as write_file:
-                json.dump(self.json_to_save, write_file, indent=4)
+            images = requests.get(url)
+            with open(f'images/{file_name}', 'wb') as file:
+                file.write(images.content)
+                bar.finish()
+        # Saving json info on selected files to our local folder
+        with open('data.json', 'w') as write_file:
+            json.dump(self.json_to_save, write_file, indent=4)
 
         return self.json_to_save
 
 
 class YaUploader(VkDownloader):
+    """
+    Class to operate with Yandex API
+    """
     host = r'https://cloud-api.yandex.net'
 
     def __init__(self, token: str):
@@ -90,7 +121,7 @@ class YaUploader(VkDownloader):
         headers = self.get_headers()
         params = {'path': path, 'overwrite': True}
         response = requests.get(url, params=params, headers=headers)
-        pprint(response.json())
+        # pprint(response.json())
         return response.json().get('href')
 
     def create_folder(self):
@@ -112,17 +143,16 @@ class YaUploader(VkDownloader):
             print('Successful Upload')
 
     def save_file(self):
-        self.sort_photos()
+        self.get_vk_photo()
         for i in self.json_to_save:
             file_name = i.get('file_name')
             url = i.get('url')
-            self.upload_file(f'/backupfolder/{file_name}.jpeg', url)
+            self.upload_file(f'/backupfolder/{file_name}', url)
 
 
 if __name__ == '__main__':
     downloader = VkDownloader(Token)
-    downloader.sort_photos()
-    # downloader.save_json()
+    downloader.get_vk_photo()
     uploader = YaUploader(Token)
-    # uploader.create_folder()
+    uploader.create_folder()
     result = uploader.save_file()
