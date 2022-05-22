8ef02d96e120677611cbfb59a91aada6110fa176
girnal82 <grinal82@gmail.com>
2022-05-22 20:36:54 +0300

    introduced uploading using requests.post via url method

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import os
 import json
 
+
 VK_USER_ID = "552934290"
 NUMBER_OF_PHOTOS = 5
 
@@ -41,7 +42,7 @@ class VkDownloader:
         in json file depending on their size
         '''
         params = {
-            'owner_id': "552934290",
+            'owner_id': VK_USER_ID,
             'access_token': self.token,
             'v': '5.131',
             'album_id': 'profile',
@@ -76,27 +77,27 @@ class VkDownloader:
 
             # Appending to resulting list (json_to_save)
             self.json_to_save.append(file_params)
-        pprint(self.json_to_save)
 
+        # pprint(self.json_to_save)
         self.create_folder()
-        """Downloading files into pre-created local folder"""
+        """Downloading files into pre-created local folder
+        """
         for file in self.json_to_save:
             file_name = file.get('file_name')
             url = file.get('url')
             # Initializing progress bar
             bar = PixelBar('Processing', max=100)
             for i in range(100):
-                time.sleep(0.001)
+                time.sleep(0.01)
                 bar.next()
             images = requests.get(url)
             with open(f'images/{file_name}', 'wb') as file:
                 file.write(images.content)
                 bar.finish()
+
         # Saving json info on selected files to our local folder
         with open('data.json', 'w') as write_file:
             json.dump(self.json_to_save, write_file, indent=4)
-
-        return self.json_to_save
 
 
 class YaUploader(VkDownloader):
@@ -116,43 +117,42 @@ class YaUploader(VkDownloader):
             'Authorization': f'OAuth {self.token}'
         }
 
-    def _get_upload_link(self, path):
-        url = f'{self.host}/v1/disk/resources/upload/'
-        headers = self.get_headers()
-        params = {'path': path, 'overwrite': True}
-        response = requests.get(url, params=params, headers=headers)
-        # pprint(response.json())
-        return response.json().get('href')
-
     def create_folder(self):
-        url = f"{self.host}/v1/disk/resources?path=backupfolder"
+        host = self.host
+        self.path = f'/images'
+        url = f"{host}/v1/disk/resources"
         headers = self.get_headers()
-        response = requests.put(url, headers=headers)
+        params = {'path': self.path}
+        response = requests.put(url, headers=headers, params=params)
         response.raise_for_status()
         if response.status_code == 201:
             print('Folder created successfully')
 
-    def upload_file(self, path, url: str):
-        upload_link = self._get_upload_link(path)
-        headers = self.get_headers()
-        params = {'path': path, 'url': url}
-        response = requests.put(
-            upload_link, headers=headers, params=params)
-        response.raise_for_status()
-        if response.status_code == 201:
-            print('Successful Upload')
-
-    def save_file(self):
-        self.get_vk_photo()
-        for i in self.json_to_save:
-            file_name = i.get('file_name')
-            url = i.get('url')
-            self.upload_file(f'/backupfolder/{file_name}', url)
+    def upload_file(self):
+        '''uploading files by their url taken from previously saved json file 
+        using requests.post method
+        '''
+        host = self.host
+        self.create_folder()
+        with open('data.json') as f:
+            data = json.load(f)
+            for i in data:
+                file_name = i.get('file_name')
+                link = i.get('url')
+                path = f'{host}/v1/disk/resources/upload'
+                disc_path = f'/images/{file_name}'
+                headers = self.get_headers()
+                params = {'path': disc_path, 'url': link}
+                response = requests.post(
+                    url=path, headers=headers, params=params)
+                if response.status_code == 202:
+                    print('Uploaded successfully')
+                else:
+                    return {'code': response.status_code, 'text': response.text}
 
 
 if __name__ == '__main__':
     downloader = VkDownloader(Token)
     downloader.get_vk_photo()
     uploader = YaUploader(Token)
-    uploader.create_folder()
-    result = uploader.save_file()
+    result = uploader.upload_file()
